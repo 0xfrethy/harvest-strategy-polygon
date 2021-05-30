fdab0278e61f604f951ff1edfc854284ef484080
JasperSlimmens <jasper@slimmens.nl>
2021-05-30 14:38:03 +0200

    Add buyback function to RewardTokenProfitNotifier and change fee to 8%

--- contracts/base/inheritance/RewardTokenProfitNotifier.sol
+++ contracts/base/inheritance/RewardTokenProfitNotifier.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "../interface/IController.sol";
+import "../interface/IFeeRewardForwarder.sol";
 import "./Controllable.sol";
 
 contract RewardTokenProfitNotifier is Controllable {
@@ -28,6 +29,7 @@ contract RewardTokenProfitNotifier is Controllable {
   }
 
   event ProfitLogInReward(uint256 profitAmount, uint256 feeAmount, uint256 timestamp);
+  event ProfitAndBuybackLog(uint256 profitAmount, uint256 feeAmount, uint256 timestamp);
 
   function notifyProfitInRewardToken(uint256 _rewardBalance) internal {
     if( _rewardBalance > 0 ){
@@ -44,5 +46,28 @@ contract RewardTokenProfitNotifier is Controllable {
       emit ProfitLogInReward(0, 0, block.timestamp);
     }
   }
+
+  function notifyProfitAndBuybackInRewardToken(uint256 _rewardBalance, address pool, uint256 _buybackRatio) internal {
+    if( _rewardBalance > 0 ){
+      uint256 feeAmount = _rewardBalance.mul(profitSharingNumerator).div(profitSharingDenominator);
+      uint256 buybackAmount = _rewardBalance.sub(feeAmount).mul(_buybackRatio).div(10000);
+
+      address forwarder = IController(controller()).feeRewardForwarder();
+      emit ProfitAndBuybackLog(_rewardBalance, feeAmount, block.timestamp);
+
+      IERC20(rewardToken).safeApprove(forwarder, 0);
+      IERC20(rewardToken).safeApprove(forwarder, _rewardBalance);
+
+      IFeeRewardForwarder(forwarder).notifyFeeAndBuybackAmounts(
+        rewardToken,
+        feeAmount,
+        pool,
+        buybackAmount
+      );
+    } else {
+      emit ProfitAndBuybackLog(0, 0, block.timestamp);
+    }
+  }
+
 
 }
